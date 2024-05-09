911451b4f8c03fcbfc8c7d7a7342cdc810f5f8b7
jeffersonnobrega <jeffersonnobrega@gmail.com>
2024-05-08 22:02:11 -0300

    atualizando progresso do apredizado

---
+++ Projetos/dotNET/RazorPagesFilmes/RazorPagesFilmes/Modelo/Filme.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesFilmes.Modelo
+{
+    public class Filme
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        [DataType(DataType.Date)]
+        public DateTime DataLancameto { get; set; }
+        public string Genero { get; set; } = string.Empty;
+        public decimal preco { get; set; } 
+    }
+}
---
+++ Projetos/dotNET/Sistema_Condominio/Form1.cs
@@ -0,0 +1,10 @@
+namespace Sistema_Condominio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
